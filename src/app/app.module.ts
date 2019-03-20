b510febc7c873fbc0dd7344e9e53144f1c3982a5
alscoundrel <alberto.r.moes@gmail.com>
2019-03-20 22:34:34 +0000

    Dia2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatDialogModule} from '@angular/material/dialog'
+
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
     AppRoutingModule
   ],
   providers: [],
